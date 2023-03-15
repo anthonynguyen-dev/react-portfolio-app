2e981b187264bdf2bb50fa5f58f3a6a3744c841a
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-03-14 21:49:50 -0500

    added cards for projects

--- src/components/Page/About.js
+++ src/components/Page/About.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>
+        The technology industry has always interested me. I found myself
+        appreciating the continuous progress that this industry goes through and
+        I am excited to contribute to it. I have many skills and attributes that
+        can positively affect this industry that I have learned over the years.
+        I have an appreciation for learning and enjoy challenging myself to
+        experience new things across many industries. I feel that this could be
+        helpful in the ever-changing world of technology.
+      </p>
+    </>
+  );
 }
 
 export default About;
--- src/components/Page/Projects.js
+++ src/components/Page/Projects.js
@@ -6,62 +6,86 @@ const styles = {
   },
 };
 
+// const renderProjects = (projects) {
+
+// }
+
 export default function Project() {
+  console.log("Project page");
   return (
     <div className="container">
       <h2>Projects</h2>
-      <ul>
-        {projectArray.map((project) => (
-          <li></li>
+      <div className="row">
+        {projectArray.map((project, index) => (
+          <div key={project.id} className="col-6">
+            <div className="card" style={{ width: "18rem" }}>
+              <div className="card-body">
+                <h5 className="card-title">{project.title}</h5>
+                <p className="card-text">{project.description}</p>
+                <a href={project.github} className="btn btn-primary">
+                  Github
+                </a>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
 
 const projectArray = [
   {
+    id: "1",
     title: "Did I take That?!",
     github: "https://github.com/Carter-Schmitz/Did_I_Take_That-",
     description:
       "An application that takes the user's medication and lets them know if the user has taken their medication.",
   },
   {
+    id: "2",
     title: "Zoo Search",
     github: "https://github.com/anthonynguyen-dev/zoo-search",
     description:
       "Lets the user look up zoos that are near the user's location.",
   },
   {
+    id: "3",
     title: "E-Commerce Back End",
     github: "https://github.com/anthonynguyen-dev/ecommerce-back-end",
     description: "",
   },
   {
+    id: "4",
     title: "PWA Text Editor",
     github: "https://github.com/anthonynguyen-dev/JATE-text-editor",
     description: "This application lets the user create text and edit it.",
   },
   {
+    id: "5",
     title: "NoSQL Social Network Api",
     github: "https://github.com/anthonynguyen-dev/noSQL-Social-Network",
     description: "",
   },
   {
+    id: "6",
     title: "Employee Generator",
     github: "https://github.com/anthonynguyen-dev/employee-generator",
     description:
       "An application that is able to let the user generate employees.",
   },
   {
+    id: "7",
     title: "Password Generator",
     github: "https://github.com/anthonynguyen-dev/password-generator",
     description:
       "An application that lets the user generate a password with any amount of characters.",
   },
   {
+    id: "8",
     title: "Note Taker App",
     github: "https://github.com/anthonynguyen-dev/note-taker-app",
-    description: "",
+    description:
+      "An application that lets the user take notes, edit them, and delete them",
   },
 ];
